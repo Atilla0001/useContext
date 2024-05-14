1b8ebb1741594d16b7b7dcd3f83d4438124e0f12
Atilla0001 <toulouse663831@gmail.com>
2024-05-14 12:34:34 +0300

    update

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -8,16 +8,17 @@ export const UserContext = createContext();
 const Page1 = () => {
 
     const [user, setUser] = useState("Atilla")
+    const [user2, setUser2] = useState("Atilla2")
 
     return (
         <div style={{ border: "2px solid black", margin: "40px 40px ", padding: "30px" }}>
 
             <h1>Page1</h1>
             
-            <p>{`atilla ${user} ${setUser}`}</p>
+            <p>{`atilla ${user} ${setUser} ${setUser2}`}</p>
 
 
-            <UserContext.Provider value={user}>
+            <UserContext.Provider value={{user,user2}}>
                 <Page2 />
             </UserContext.Provider>
 
--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -3,12 +3,14 @@ import  {useContext} from "react"
 import {UserContext} from './Page1'
 
 const Page4 = () => {
-  const user = useContext(UserContext)
+  const {user, user2} = useContext(UserContext)
+
   return (
     <div style={{ border:"2px solid black", margin:"40px 40px ",padding:"30px"}}>
 
       <h1>Page4</h1>
       <p>{`${user}`}</p>
+      <p>{`${user2}`}</p>
     </div>
   )
 }
